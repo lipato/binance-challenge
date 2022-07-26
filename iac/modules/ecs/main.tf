24d88e094eb57243b84770398b7e749809d71d6e
George Lipatov <george.lipatov@clearscale.com>
2022-07-27 00:36:34 +0800

    and vpc endpoint, fixed sg

--- iac/modules/ecs/main.tf
+++ iac/modules/ecs/main.tf
@@ -2,5 +2,44 @@ resource "aws_ecs_cluster" "this" {
   name = "ecs-cluster"
 }
 
+resource "aws_vpc_endpoint" "ecr-dkr" {
+  vpc_id              = var.vpc_id
+  auto_accept         = true
+  private_dns_enabled = true
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids = var.ecs_security_groups
+  subnet_ids = var.private_subnets
+}
+
+resource "aws_vpc_endpoint" "ecr-api" {
+  vpc_id              = var.vpc_id
+  auto_accept         = true
+  private_dns_enabled = true
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids = var.ecs_security_groups
+  subnet_ids = var.private_subnets
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = var.vpc_id
+  auto_accept         = true
+  private_dns_enabled = true
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.logs"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids = var.ecs_security_groups
+  subnet_ids = var.private_subnets
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id              = var.vpc_id
+  auto_accept         = true
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids = var.ecs_security_groups
+  subnet_ids = var.private_subnets
+}
+
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
--- iac/modules/network/sg.tf
+++ iac/modules/network/sg.tf
@@ -4,13 +4,6 @@ resource "aws_security_group" "ecs-sg" {
   vpc_id      = aws_vpc.this.id
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    self             = true
-  }
-
-  ingress {
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
@@ -19,11 +12,31 @@ resource "aws_security_group" "ecs-sg" {
     ]
   }
 
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [
+      "0.0.0.0/0"
+    ]
+  }
+
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    security_groups      = [
+      aws_security_group.pub-alb-sg.id
+    ]
+  }
+
+  egress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [
+      "0.0.0.0/0"
+    ]
   }
 }
 
